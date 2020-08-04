4034e235b3204db573f2b50a9eeec4fc9574d229
AffluentAvo <diemcletsplay@gmail.com>
2020-08-04 22:55:22 +0200

    Improved memory usage Added permission checks on command use Fixed Unknown Message error Added console and stop command some more

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,10 +1,8 @@
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.*;
 
 public class Game {
-    Message gameMessage;
+    long gameMessageID;
+    long channelID;
     User user;
     String playerEmote = ":flushed:";
     public boolean gameActive = false;
@@ -12,67 +10,59 @@ public class Game {
     int width = 9;
     int height = 6;
     Grid grid = new Grid(width, height, level, playerEmote);
-    public Game(User user)
-    {
+
+    public Game(User user) {
         this.user = user;
     }
-    public void setPlayerEmote(String emote)
-    {
+
+    public void setPlayerEmote(String emote) {
         playerEmote = emote;
     }
-    public void setGameMessage(Message message)
-    {
-        gameMessage = message;
+
+    public void setGameMessage(Message gameMessage) {
+        // To avoid an Unknown Message error, we will store the IDs and retrieve the Channel object when needed.
+        gameMessageID = gameMessage.getIdLong();
+        channelID = gameMessage.getChannel().getIdLong();
     }
-    public void newGame(MessageChannel channel)
-    {
-        if (!gameActive)
-        {
+
+    public void newGame(MessageChannel channel) {
+        if (!gameActive) {
             level = 1;
             width = 9;
             height = 6;
             grid = new Grid(width, height, level, playerEmote);
-
             gameActive = true;
             Commands.sendGameEmbed(channel, String.valueOf(level), grid.toString(), user);
-
         }
     }
-    public void run(Guild guild, MessageChannel channel, String userInput)
-    {
-        if (userInput.equals("stop") && gameActive)
-        {
-           channel.sendMessage("Thanks for playing, " + user.getAsMention() + "!").queue();
-           gameActive = false;
+
+    public void run(Guild guild, TextChannel channel, String userInput) {
+        if (userInput.equals("stop") && gameActive) {
+            channel.sendMessage("Thanks for playing, " + user.getAsMention() + "!").queue();
+            gameActive = false;
         }
 
-        if (userInput.equals("play") && !gameActive)
-        {
+        if (userInput.equals("play") && !gameActive) {
             newGame(channel);
-        }
-        else if (gameActive)
-        {
+        } else if (gameActive) {
             if (!grid.hasWon()) {
-
                 String direction = userInput;
-                if (direction.equals("up") || direction.equals("w"))
-                {
+                if (direction.equals("up") || direction.equals("w")) {
                     grid.getPlayer().moveUp();
-                } else if (direction.equals("down") || direction.equals("s"))
-                {
+                } else if (direction.equals("down") || direction.equals("s")) {
                     grid.getPlayer().moveDown();
-                } else if (direction.equals("left") || direction.equals("a"))
-                {
+                } else if (direction.equals("left") || direction.equals("a")) {
                     grid.getPlayer().moveLeft();
-                } else if (direction.equals("right") || direction.equals("d"))
-                {
+                } else if (direction.equals("right") || direction.equals("d")) {
                     grid.getPlayer().moveRight();
-                } else if (direction.equals("r"))
-                {
+                } else if (direction.equals("r")) {
                     grid.reset();
                 }
-                if (!grid.hasWon()) { //need to check again
-                    Commands.updateGameEmbed(gameMessage, String.valueOf(level), grid.toString(), user);
+                if (!grid.hasWon()) {
+                    TextChannel textChannel = Bot.getShardManager().getTextChannelById(channelID);
+                    if (textChannel != null) {
+                        textChannel.retrieveMessageById(gameMessageID).queue(gameMessage -> Commands.updateGameEmbed(gameMessage, String.valueOf(level), grid.toString(), user));
+                    }
                 }
             }
             if (grid.hasWon()) {
@@ -80,13 +70,16 @@ public class Game {
                 if (width < 13) {
                     width += 2;
                 }
-                if (height < 8)
-                {
+                if (height < 8) {
                     height += 1;
                 }
-                Commands.sendWinEmbed(guild, gameMessage, String.valueOf(level));
+                TextChannel textChannel = Bot.getShardManager().getTextChannelById(channelID);
+                if (textChannel != null) {
+                    textChannel.retrieveMessageById(gameMessageID).queue(gameMessage -> Commands.sendWinEmbed(guild,
+                            gameMessage, String.valueOf(level)));
+                }
                 grid = new Grid(width, height, level, playerEmote);
             }
-            }
         }
+    }
 }
