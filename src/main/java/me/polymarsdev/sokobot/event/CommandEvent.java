0a66e80087dcb4d4d6bd443628044654fcb70ae3
mandar1jn <marijnkneppers@gmail.com>
2020-08-27 18:48:35 +0200

    added A LOT of unused functions

--- src/main/java/me/polymarsdev/sokobot/event/CommandEvent.java
+++ src/main/java/me/polymarsdev/sokobot/event/CommandEvent.java
@@ -1,13 +1,9 @@
 package me.polymarsdev.sokobot.event;
 
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Consumer;
 
 public class CommandEvent {
     private static final int MAX_MESSAGES = 2;
@@ -24,90 +20,18 @@ public class CommandEvent {
         return args;
     }
 
-    public GuildMessageReceivedEvent getEvent() {
-        return event;
-    }
-
-    public List<Member> getMentionedMembers() {
-        List<Member> mentionedMembers = new ArrayList<>(event.getMessage().getMentionedMembers());
-        if (event.getMessage().getContentRaw().startsWith("<@!" + event.getJDA().getSelfUser().getId() + ">"))
-            mentionedMembers.remove(0);
-        return mentionedMembers;
-    }
-
     public void reply(String message) {
         sendMessage(event.getChannel(), message);
-    }
-
-    public void reply(String message, Consumer<Message> success) {
-        sendMessage(event.getChannel(), message, success);
-    }
-
-    public void reply(String message, Consumer<Message> success, Consumer<Throwable> failure) {
-        sendMessage(event.getChannel(), message, success, failure);
     }
 
     public void reply(MessageEmbed embed) {
         event.getChannel().sendMessage(embed).queue();
     }
 
-    public void reply(MessageEmbed embed, Consumer<Message> success) {
-        event.getChannel().sendMessage(embed).queue(success);
-    }
-
-    public void reply(MessageEmbed embed, Consumer<Message> success, Consumer<Throwable> failure) {
-        event.getChannel().sendMessage(embed).queue(success, failure);
-    }
-
-    public void reply(Message message) {
-        event.getChannel().sendMessage(message).queue();
-    }
-
-    public void reply(Message message, Consumer<Message> success) {
-        event.getChannel().sendMessage(message).queue(success);
-    }
-
-    public void reply(Message message, Consumer<Message> success, Consumer<Throwable> failure) {
-        event.getChannel().sendMessage(message).queue(success, failure);
-    }
-
-    public void reply(File file, String filename) {
-        event.getChannel().sendFile(file, filename).queue();
-    }
-
-    public void reply(String message, File file, String filename) {
-        String msg = message == null ? null : splitMessage(message).get(0);
-        if (msg == null) event.getChannel().sendFile(file, filename).queue();
-        else event.getChannel().sendMessage(msg).addFile(file, filename).queue();
-    }
-
     private void sendMessage(MessageChannel chan, String message) {
         ArrayList<String> messages = splitMessage(message);
         for (int i = 0; i < MAX_MESSAGES && i < messages.size(); i++) {
             chan.sendMessage(messages.get(i)).queue();
-        }
-    }
-
-    private void sendMessage(MessageChannel chan, String message, Consumer<Message> success) {
-        ArrayList<String> messages = splitMessage(message);
-        for (int i = 0; i < MAX_MESSAGES && i < messages.size(); i++) {
-            if (i + 1 == MAX_MESSAGES || i + 1 == messages.size()) {
-                chan.sendMessage(messages.get(i)).queue(success);
-            } else {
-                chan.sendMessage(messages.get(i)).queue();
-            }
-        }
-    }
-
-    private void sendMessage(MessageChannel chan, String message, Consumer<Message> success,
-                             Consumer<Throwable> failure) {
-        ArrayList<String> messages = splitMessage(message);
-        for (int i = 0; i < MAX_MESSAGES && i < messages.size(); i++) {
-            if (i + 1 == MAX_MESSAGES || i + 1 == messages.size()) {
-                chan.sendMessage(messages.get(i)).queue(success, failure);
-            } else {
-                chan.sendMessage(messages.get(i)).queue();
-            }
         }
     }
 
@@ -129,20 +53,12 @@ public class CommandEvent {
         return msgs;
     }
 
-    SelfUser getSelfUser() {
-        return event.getJDA().getSelfUser();
-    }
-
     public User getAuthor() {
         return event.getAuthor();
     }
 
     public Guild getGuild() {
         return event.getGuild();
-    }
-
-    public JDA getJDA() {
-        return event.getJDA();
     }
 
     public Member getMember() {
